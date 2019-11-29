ff04ed85320427dde5b9de1502da0a8f07b27107
Ranjith <ranju08134@gmail.com>
2019-11-29 20:39:56 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -9,12 +9,14 @@ import * as firebase from 'firebase'
 import registerServiceWorker from './registerServiceWorker';
 const store = configureStore();
 var config = {
-  apiKey: "AIzaSyB4gVY_Cpw5khJjvWhK5MF7pwh6x7V9jaw",
-    authDomain: "auction-app-7b58a.firebaseapp.com",
-    databaseURL: "https://auction-app-7b58a.firebaseio.com",
-    projectId: "auction-app-7b58a",
-    storageBucket: "auction-app-7b58a.appspot.com",
-    messagingSenderId: "431936014331"
+  apiKey: "AIzaSyCTsQ99HjLa9BtHk7xSWSofwoWkRaZABhI",
+  authDomain: "bidnow-9f2b3.firebaseapp.com",
+  databaseURL: "https://bidnow-9f2b3.firebaseio.com",
+  projectId: "bidnow-9f2b3",
+  storageBucket: "bidnow-9f2b3.appspot.com",
+  messagingSenderId: "1001875005960",
+  appId: "1:1001875005960:web:98988469735365485de7bd",
+  measurementId: "G-KK1HBT9S1H"
 };
   firebase.initializeApp(config);
 ReactDOM.render(<MuiThemeProvider >
